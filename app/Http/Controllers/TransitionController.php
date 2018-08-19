f55f939f508edc81d21ea6d7728f8dddd9eb3dff
Hussein Mirzaki <husseinmirzaki@gmail.com>
2018-08-19 19:35:21 +0430

    session 1 - Project initialized and models are created

---
+++ app/Http/Controllers/TransitionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Transition;
+use Illuminate\Http\Request;
+
+class TransitionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Transition  $transition
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Transition $transition)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Transition  $transition
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Transition $transition)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Transition  $transition
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Transition $transition)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Transition  $transition
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Transition $transition)
+    {
+        //
+    }
+}
